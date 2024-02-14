4145249db5db5cf513899d2b52f4a7a39ecad165
LAAOUINI Haitam <107327154+laaouinihaitam@users.noreply.github.com>
2024-02-14 03:10:04 +0100

    Update apli4.py

--- apli4.py
+++ apli4.py
@@ -24,7 +24,7 @@ names=["Mohammed Rahmouni","Mourad Bouchnaf","Haitam Laaouini"]
 usernames =["rahmouni","bouchnaf","laaouini"]
 
 # load hashed passwords
-file_path = Path("C:/Users/LOGION/Desktop/haiiitam/hashed_pw.pkl")
+file_path = Path(__file__).parent /"hashed_pw.pkl"
 with file_path.open("rb") as file :
     hashed_passwords = pickle.load(file)
 authenticator = stauth.Authenticate(names, usernames, hashed_passwords,"excel_plotter","abcdef",0)
